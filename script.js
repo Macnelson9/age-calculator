4e86e22c37fcb9e17abc2fe1276fe17d13849531
Macnelson9 <michaelofatu@gmail.com>
2025-01-12 23:18:39 +0100

    Added Features v2.0 For this commit, I have added the resources to implement the zodiac sign feature for the app. I intend for users to be able to get information on their personalities while checking their ages. I will write the logic to get that working soon. But the styling and skeleton work is done already. Happy Coding!!

--- script.js
+++ script.js
@@ -8,6 +8,45 @@ const daysEl = document.querySelector('.days-bold');
 const yearsP = document.querySelector('.years-p');
 const monthsP = document.querySelector('.months-p');
 const daysP = document.querySelector('.days-p');
+const zodiacName = document.getElementById('sign');
+const zodiacCharacteristics = document.getElementById('characteristics');
+const zodiacDetails = document.getElementById('more-details');
+const zodiacSummary = document.getElementById('summary');
+const zodiacImage = document.getElementById('zodiacImage');
+
+const ageFormatter = function (age, month, day) {
+  if (age <= 1) {
+    yearsP.innerText = 'Year';
+  } else {
+    yearsP.innerText = 'Years';
+  }
+
+  if (Math.abs(month) <= 1) {
+    monthsP.innerText = 'Month';
+  } else {
+    monthsP.innerText = 'Months';
+  }
+
+  if (Math.abs(day) <= 1) {
+    daysP.innerText = 'Day';
+  } else {
+    daysP.innerText = 'Days';
+  }
+};
+
+// const zodiacSign = function (month, day) {
+//   month = Math.abs(month) + 1;
+//   day = Math.abs(day);
+
+//   console.log(month, day);
+
+//   if (month >= 3 && month <= 4 && day >= 21 && day <= 19) {
+//     zodiacName.innerHTML = `<li id="sign"><strong>Zodiac Sign:</strong> Aries</li>`;
+//     zodiacCharacteristics.innerHTML = `<li id="characteristics">
+//             <strong>Characteristics:</strong> Eager, Dynamic , quick and competitive.
+//           </li>`;
+//   }
+// };
 
 calcBtn.addEventListener('click', function () {
   const currentDate = new Date();
@@ -27,23 +66,9 @@ calcBtn.addEventListener('click', function () {
   monthsEl.innerText = Math.abs(month);
   daysEl.innerText = Math.abs(day);
 
-  if (age <= 1) {
-    yearsP.innerText = 'Year';
-  } else {
-    yearsP.innerText = 'Years';
-  }
+  ageFormatter(age, month, day);
 
-  if (Math.abs(month) <= 1) {
-    monthsP.innerText = 'Month';
-  } else {
-    monthsP.innerText = 'Months';
-  }
+  //   zodiacSign(month, birthDate.getDate());
 
-  if (Math.abs(day) <= 1) {
-    daysP.innerText = 'Day';
-  } else {
-    daysP.innerText = 'Days';
-  }
-
-  return age;
+  return age, month, day;
 });
